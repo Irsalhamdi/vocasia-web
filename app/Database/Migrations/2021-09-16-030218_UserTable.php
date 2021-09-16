7c5e147ceb80dcebb1f29d4ac1b22660bcbf135b
irhas Alba <irhasalba1998@gmail.com>
2021-09-16 10:26:05 +0700

    added users migration

---
+++ app/Database/Migrations/2021-09-16-030218_UserTable.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class UserTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id_user' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'auto_increment' => true
+            ],
+            'first_name' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'last_name' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'email' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'password' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'username' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'role_id' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'is_verified' => [
+                'type' => 'ENUM',
+                'constraint' => ['active', 'deactive'],
+                'default' => 'deactive'
+            ],
+        ]);
+
+        $this->forge->addKey('id_user', true);
+        $this->forge->createTable('users');
+    }
+
+    public function down()
+    {
+        $this->forge->dropDatabase('users', true);
+    }
+}
