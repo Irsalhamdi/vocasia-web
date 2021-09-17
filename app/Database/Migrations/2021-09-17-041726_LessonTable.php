7b9ae44d68016a843b20cfe1d6bf0b3923a58b94
irhas Alba <irhasalba1998@gmail.com>
2021-09-17 11:18:12 +0700

    change order migration

---
+++ app/Database/Migrations/2021-09-17-041726_LessonTable.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class LessonTable extends Migration
+{
+    public function up()
+    {
+        //
+    }
+
+    public function down()
+    {
+        //
+    }
+}
