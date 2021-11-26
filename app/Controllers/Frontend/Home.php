da3086e6868b1052216c8b4e7aa2121e1f545131
irhas Alba <irhasalba1998@gmail.com>
2021-11-26 10:55:30 +0700

    fixing key iteral date

--- app/Controllers/Frontend/Home.php
+++ app/Controllers/Frontend/Home.php
@@ -693,18 +693,18 @@ class Home extends FrontendController
         $exp = explode('-', $get_date);
         $date = substr($exp[2], 0, 2);
         $month_in_bahasa = [
-            1 => 'Januari',
-            2 => 'Februari',
-            3 => 'Maret',
-            4 => 'April',
-            5 => 'Mei',
-            6 => 'Juni',
-            7 => 'Juli',
-            8 => 'Agustus',
-            9 => 'September',
-            10 => 'Oktober',
-            11 => 'November',
-            12 => 'Desember'
+            "01" => 'Januari',
+            "02" => 'Februari',
+            "03" => 'Maret',
+            "04" => 'April',
+            "05" => 'Mei',
+            "06" => 'Juni',
+            "07" => 'Juli',
+            "08" => 'Agustus',
+            "09" => 'September',
+            "10" => 'Oktober',
+            "11" => 'November',
+            "12" => 'Desember'
 
         ];
         $month = $exp[1];
